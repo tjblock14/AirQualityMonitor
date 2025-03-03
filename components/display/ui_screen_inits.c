c53ec5f45a516049bc16e795577674231b2a0b5d
tjblock14 <trevorblock19@gmail.com>
2025-03-03 12:08:08 -0600

    display - work in progress

--- components/display/ui_screen_inits.c
+++ components/display/ui_screen_inits.c
@@ -52,7 +52,7 @@ void temp_humid_screen_init()
 void co2_screen_init()
 {
     // Clear screen
-    err = i2c_master_transmit(i2c_display_device_handle, clear_display_cmd, sizeof(clear_display_cmd), pdMS_TO_TICKS(100));
+    err = i2c_master_transmit(i2c_display_device_handle, clear_display_cmd, sizeof(clear_display_cmd), pdMS_TO_TICKS(1000));
     if(err == ESP_OK)
     {
         ESP_LOGI(TAG, "Cleared Screen");
@@ -63,7 +63,7 @@ void co2_screen_init()
         ESP_LOGW(TAG, "Error is 0x%02X", err);
     }
 
-    vTaskDelay(pdMS_TO_TICKS(10));
+    vTaskDelay(pdMS_TO_TICKS(100));
 
     //get average CO2 level from last 10 readings
     uint16_t co2_level = get_co2_level_for_display();
@@ -71,7 +71,7 @@ void co2_screen_init()
     reset_text_buffers();
     snprintf(display_text_buf_line1, sizeof(display_text_buf_line1), "CO2: %dppm", co2_level);
 
-    err = i2c_master_transmit(i2c_display_device_handle, (uint8_t *)display_text_buf_line1, strlen(display_text_buf_line1), pdMS_TO_TICKS(100));
+    err = i2c_master_transmit(i2c_display_device_handle, (uint8_t *)display_text_buf_line1, strlen(display_text_buf_line1), pdMS_TO_TICKS(1000));
     if(err != ESP_OK)
     {
         ESP_LOGE(TAG, "Error sending string to screen");
